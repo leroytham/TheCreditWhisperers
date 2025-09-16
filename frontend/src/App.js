3b46c233032b0e8b693b32066023111eb5621071
WenXiang <46016758+wenxiang1999@users.noreply.github.com>
2025-09-16 19:28:59 +0800

    Push everything except .env

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import SignupCard from "./SignupCard";
+import LoginCard from "./LoginPage";
+import Dashboard from "./new_dashboard";
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/signup">Signup</Link> | <Link to="/login">Login</Link>
+      </nav>
+      <Routes>
+        <Route path="/signup" element={<SignupCard />} />
+        <Route path="/login" element={<LoginCard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<SignupCard />} /> 
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
